4e03f16a95bb7483ad18f52cd706ab874e05b117
EL Putnam <63127590+fleshcircuit@users.noreply.github.com>
2021-07-14 11:27:07 +0100

    Still debugging
    Loading static json file in setup

--- sketch.js
+++ sketch.js
@@ -32,9 +32,9 @@ function setup() {
   j = 0;
   //console.log(list1);
   let day = int(random(0,131));
-  heartRate = loadJSON(list1[day]);
+  //heartRate = loadJSON(list1[day]);
   console.log(list1[day])
-  //heartRate = loadJSON('data/heart_rate-2020-05-01.json')
+  heartRate = loadJSON('data/heart_rate-2020-05-01.json')
   
   for (let i = 0; i < num; i++){
     swarm.push(new Element())
