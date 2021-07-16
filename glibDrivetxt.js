a3f27a9f4f1171256ab83170fff90d702f35f96a
elputnam <elputnam@inaction.ie>
2021-07-16 14:26:02 +0100

    Initial Commit

--- glibDrivetxt.js
+++ glibDrivetxt.js
@@ -1,10 +1,11 @@
 function glibDriveText(){
-    if (frameCount%15==0 ){
+    if (frameCount%19==0 ){
     textSize(55);
     noStroke();
     fill(200);
     textFont('VT323');
-    text(txt[j], width*.05, height*.8);
+    voice.speak(txt1[j]);
+    text(txt2[j], width*.05, height*.8);
     j += 1;
     }
 }
