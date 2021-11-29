46225a98f57076aa09d3a09d1bc4e6507fe81a44
fleshcircuit <elputnam@inaction.ie>
2021-11-29 15:32:18 +0000

    initial commit

--- js/glibDrivetxt.js
+++ js/glibDrivetxt.js
@@ -1,10 +1,9 @@
 function glibDriveText(){
     if (frameCount%19==0 ){
-    textSize(55);
+    textSize(75);
     noStroke();
     fill(200);
     textFont('VT323');
-    voice.speak(txt1[j]);
     text(txt2[j], width*.05, height*.8);
     j += 1;
     }
--- js/sketch.js
+++ js/sketch.js
@@ -7,17 +7,22 @@ let j = 0;
 let colA = 0;
 let colB = 300;
 let tau = 0;
-var voice = new p5.Speech();
 
 let swarm = [];
 var num;
 //let H3 = 0;
 
-let link;
-let next; 
+// let link;
+// let next; 
 //let link1;
 //let link2;
 //let link3;
+
+var capturer = new CCapture({
+  format:'webm', 
+  framerate: 12
+});
+var btn1;
 
 
 function preload(){
@@ -29,9 +34,10 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // createCanvas(windowWidth, windowHeight);
+  createCanvas(1920, 1080);
   colorMode(HSB, 360, 100, 100, 100);
-  frameRate(15);
+  frameRate(12);
   num = height*.3;
   j = 0;
   //console.log(list1);
@@ -49,21 +55,27 @@ function setup() {
 
   
   //button
-  link = createA('https://fleshcircuit.github.io/AtTheEndOf/', '');
+  // link = createA('https://fleshcircuit.github.io/AtTheEndOf/', '');
   //link1 = createA('http://127.0.0.1:5501/PeatBramble/', '');
   //link2 = createA('http://127.0.0.1:5501/AtTheEndOf/', '');
+
+  btn1 = document.createElement('button');
+  btn1.textContent = "save recording";
+  document.body.appendChild(btn1);
+  btn1.onclick = save_record;
 }
 
 function draw() {
   background(random(30), 10);
+  if (frameCount==1) capturer.start(); // start the animation capture
   //heartFetch();
   // debugger
   print(frameCount);
  
   if (frameCount >= 350){
     bpm = heartRate[B].value['bpm'];
-    colA = map(bpm, 60, 170, 0, 360);
-    colB = map(bpm, 60, 170, 360, 0);
+    colA = map(bpm, 60, 170, 0, 175);
+    colB = map(bpm, 60, 170, 175, 0);
     B += 1;
 
     push();
@@ -86,10 +98,10 @@ function draw() {
       link = link2;
     }*/  
 
-if (frameCount == 2300){
-  next = createButton('next').parent(link); 
-  next.position(width*.1, height*.1);
-}
+// if (frameCount == 2300){
+//   next = createButton('next').parent(link); 
+//   next.position(width*.1, height*.1);
+// }
 
 
 
@@ -97,10 +109,11 @@ if (frameCount == 2300){
   for (let i = 0; i < swarm.length; i++){
     noStroke();
     //color blend based on mouse location
-    let col1 = map(mouseX, 0, width, 0, 360);
-    let col2 = map(mouseY, 0, height, 360, 0);
-    let blender = map(mouseX, 0, width, 0, 1);
-    let H3 = lerp(col1, col2, blender);
+    // let col1 = map(mouseX, 0, width, 0, 360);
+    // let col2 = map(mouseY, 0, height, 360, 0);
+    // let blender = map(mouseX, 0, width, 0, 1);
+    // let H3 = lerp(col1, col2, blender);
+    let H3 = map(colA, 0, 175, 175, 360);
     fill(H3, random(100), random(100))
     swarm[i].oscillate();
     swarm[i].display();
@@ -114,7 +127,12 @@ if (frameCount == 2300){
   if (frameCount >= 100){
     glibDriveText();
   }
-  
+
+  capturer.capture(document.getElementById('defaultCanvas0')); 
+  if (frameCount==3000){
+    save_record();
+  }
+
 }
 
 
@@ -161,6 +179,12 @@ class Element{
   display() {
     let x = sin(this.angle.x) * this.amp.x;
     let y = sin(this.angle.y) * this.amp.y;
-    ellipse(mouseX + x, mouseY + y, random(10));
+    // ellipse(mouseX + x, mouseY + y, random(10));
+    ellipse(width/2 + x, height/2 + y, random(10));
  }
 }
+
+function save_record() {
+  capturer.save();
+}
+
